9f8a3e081216b1864ffdac51d1cf8031f7944e50
Dustin Sallings <dustin@spy.net>
2011-04-21 02:52:07 -0700

    Added loss data.

---
+++ evently/lossdist/_init/data.js
@@ -0,0 +1,15 @@
+function(data) {
+    var app = $$(this).app;
+    var path = app.require("vendor/couchapp/lib/path").init(app.req);
+
+    return {
+        results: data.rows.map(function(r) {
+            var k = r.key;
+            var count = r.value;
+            return {
+                failures: k[1],
+                loss: k[2],
+                count: count
+            };
+        })};
+}
---
+++ evently/lossdist/_init/query.js
@@ -0,0 +1,10 @@
+function() {
+    var docid = $$("#testinfo").docid;
+    return {
+        view : "counts",
+        endkey : [docid, {}],
+        startkey : [docid],
+        group: true,
+        reduce: true
+    };
+};
