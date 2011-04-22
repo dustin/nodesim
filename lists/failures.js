0d05a6b3959c39f83eb735235cc7431d9815c270
Dustin Sallings <dustin@spy.net>
2011-04-22 10:21:23 -0700

    Better display of failure lists.

--- lists/failures.js
+++ lists/failures.js
@@ -15,20 +15,44 @@ function(head, req) {
         var path = require("vendor/couchapp/lib/path").init(req);
 
         send(Mustache.to_html(templates.head, data));
-        send("<h1>List of Failures</h1><ul>");
+        send("<h1>List of Failures</h1>");
 
-        while(row = getRow()) {
-            send(Mustache.to_html(templates.failure, {
+        var by_size = {};
+
+        while( (row = getRow()) ) {
+            var len = row.value.missing.length;
+            if (!by_size[len]) {
+                by_size[len] = [];
+            }
+            by_size[len].push({
                 id: row.id,
                 test: row.key[0],
-                n_dead_nodes: row.key[1],
+                n_dead_nodes: parseInt(row.key[1]),
+                dead_str: row.value.failed.join(", "),
                 n_missing: row.value.missing.length,
                 failed: row.value.failed,
                 missing: row.value.missing,
                 show: path.show('scenario', row.id)
-            }));
+            });
         }
-        send("</ul>");
+
+        var sizes = [];
+        for (var k in by_size) {
+            sizes.push([k, by_size[k].sort(function (a,b) {
+                return a.n_dead_nodes - b.n_dead_nodes;
+            })]);
+        }
+        sizes.sort(function(a, b) { return a[0] - b[0];});
+
+        for (var i = 0; i < sizes.length; ++i) {
+            send("<h2>Losing " + sizes[i][0] + " VBuckets</h2><ul>");
+            var failures = sizes[i][1];
+            for (var j = 0; j < failures.length; ++j) {
+                send(Mustache.to_html(templates.failure, failures[j]));
+            }
+            send("</ul>");
+        }
+
         send(Mustache.to_html(templates.tail, data));
     });
 }
--- views/find_missing/map.js
+++ views/find_missing/map.js
@@ -1,10 +1,14 @@
 function(doc) {
   if (doc.missing && doc.missing.length > 0) {
+      var failed = doc.failed.map(function(n) {
+          return parseInt(n.id);
+      }).sort(function (a, b) { return a - b; });
       emit([doc.test, doc.n_dead_nodes],
            {"algorithm": doc.algorithm,
             "missing": doc.missing,
             "n_missing": doc.missing.length,
             "down": doc.n_dead_nodes,
-            "up": doc.n_alive_nodes});
+            "up": doc.n_alive_nodes,
+            "failed": failed});
   }
 }
