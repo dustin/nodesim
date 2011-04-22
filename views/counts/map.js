fabdc63e8baf1669a25bff5652f724b90980d11d
Dustin Sallings <dustin@spy.net>
2011-04-21 22:05:19 -0700

    Emit more stuff from the count mapper.

--- views/counts/map.js
+++ views/counts/map.js
@@ -1,5 +1,7 @@
 function(doc) {
   if (doc.n_alive_nodes) {
-    emit([doc.test, doc.n_dead_nodes, doc.missing.length], 1);
+    emit([doc.test, doc.n_dead_nodes, doc.missing.length],
+         {failed: doc.failed,
+          missing: doc.missing});
   }
 }
