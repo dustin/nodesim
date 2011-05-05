59fdd1b10dd8bdd71ad498b34efced46f56f0a47
Dustin Sallings <dustin@spy.net>
2011-05-04 21:41:01 -0700

    [wip] Replacing evently.

--- _attachments/js/prefix-loader.js
+++ _attachments/js/prefix-loader.js
@@ -7,8 +7,7 @@ function couchapp_prefix_load(prefix) {
         "/_utils/script/jquery.couch.js",
         prefix + "vendor/couchapp/jquery.couch.app.js",
         prefix + "vendor/couchapp/jquery.couch.app.util.js",
-        prefix + "vendor/couchapp/jquery.mustache.js",
-        prefix + "vendor/couchapp/jquery.evently.js"
+        prefix + "vendor/couchapp/jquery.mustache.js"
         ];
     for (var i=0; i < scripts.length; i++) {
         document.write('<script src="'+scripts[i]+'"><\/script>')
