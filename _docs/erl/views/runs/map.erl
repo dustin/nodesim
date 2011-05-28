46af21b4b7209f512e46ec25715d7650ac645e4d
Dustin Sallings <dustin@spy.net>
2011-05-28 02:04:32 -0700

    Slight index trimming.

--- _docs/erl/views/runs/map.erl
+++ _docs/erl/views/runs/map.erl
@@ -2,7 +2,7 @@ fun({Doc}) ->
         F = fun(B) -> proplists:get_value(B, Doc) end,
         case F(<<"type">>) of
             <<"test">> ->
-                Emit([F(<<"algorithm">>), F(<<"start_time">>), F(<<"_id">>)], {Doc});
+                Emit([F(<<"algorithm">>), F(<<"start_time">>), F(<<"_id">>)], null);
             _ -> false
         end
 end.
